a3504ee6453113682dbc3070bc6bd1651c80c3ce
sachinreddy1 <sachinreddy1009@gmail.com>
2019-07-06 00:32:14 -0400

    Game Over UI added.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool gameEnded;
+    public GameObject gameOverUI;
+    public GameObject completeLevelUI;
+
+    void Start () {
+        gameEnded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameEnded)
+            return;
+
+        if (Input.GetKeyDown("e"))
+        {
+            EndGame();
+        }
+
+        if (PlayerStats.Lives <= 0) {
+            EndGame();
+        }
+    }
+
+    private void EndGame () {
+        gameEnded = true;
+        Debug.Log("GAME OVER.");
+        gameOverUI.SetActive(true);
+    }
+
+    public void WinLevel() {
+        gameEnded = true;
+        completeLevelUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Text roundsText;
+    public SceneFader sceneFader;
+    public string menuSceneName = "MainMenu";
+
+    void OnEnable () {
+        roundsText.text = PlayerStats.Rounds.ToString();
+    }
+
+    public void Retry () {
+        sceneFader.FadeTo(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu() {
+        sceneFader.FadeTo(menuSceneName);
+    }
+}
