5b2b340f255d455d21e2f37e37f36d68d040eddc
sachinreddy1 <sachinreddy1009@gmail.com>
2019-07-06 01:01:09 -0400

    Paused menu added.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+    public SceneFader sceneFader;
+
+    public string menuSceneName = "MainMenu";
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
+            Toggle();
+        }
+    }
+
+    public void Toggle() {
+        ui.SetActive(!ui.activeSelf);
+        if (ui.activeSelf) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry() {
+        Toggle();
+        sceneFader.FadeTo(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu() {
+        Toggle();
+        sceneFader.FadeTo(menuSceneName);
+    }
+}
