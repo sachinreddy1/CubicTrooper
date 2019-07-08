89cb5ffda644950bac69616ec86d0319d1906b4b
sachinreddy1 <sachinreddy1009@gmail.com>
2019-07-08 06:37:00 -0400

    psycho mantis?

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopUI;
+    public Animator shopUI_;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        shopUI_ = shopUI.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowUI();
+    }
+
+    void ShowUI() {
+        Vector3 dir = player.position - transform.position;
+        if (Vector3.Distance(transform.position, player.position) <= 4f) {
+            // Disable Shooting
+            shopUI_.SetBool("inRange", true);
+        }
+        else {
+            // Enable Shooting
+            shopUI_.SetBool("inRange", false);
+        }
+    }
+}
