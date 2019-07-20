a4a98b02d0e1c75565b7c289fcf3a9e849583e99
sachinreddy1 <sachinreddy1009@gmail.com>
2019-07-19 23:42:21 -0400

    MainMenu stuff

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneFader sceneFader;
+
+    void Start() {
+
+    }
+
+    public void Play() {
+        LoadLevel("Level_01");
+    }   
+
+    public void LoadLevel(string levelName){
+        sceneFader.FadeTo(levelName);
+    }
+
+
+    public void Quit() {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+}
